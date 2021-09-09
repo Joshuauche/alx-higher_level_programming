51b6ce21b89b0f0cee766e9534da4f4cbc0bd4df
joshua onwuka <contactuche02@gmail.com>
2021-09-09 10:15:13 -0700

    updated

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    num = len(argv)
+    num = len(sys.argv)
     if num == 1:
         print("{} arguments".format(num - 1))
     elif num == 2:
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments".format(num - 1))
     for i in range(1, num):
-        print("{}: {}".format(i, argv[i]))
+        print("{}: {}".format(i, sys.argv[i]))
