aaa8cdf59686f3dd7c08518243b9da48f1f61d8b
joshua onwuka <contactuche02@gmail.com>
2021-09-28 08:21:58 -0700

    defines a square and returns the current square area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+class Square:
+    """Defines a square.
+    Private instance attribute: size.
+    Instantiate with optional size.
+    """
+    def __init__(self, size=0):
+        """Initialize the data conditions"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size__ = size
+
+    def area(self):
+        return self.__size__ ** 2
