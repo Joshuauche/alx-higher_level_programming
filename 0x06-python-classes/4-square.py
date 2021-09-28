bed4c14ba41d5d6556533fff2b2f9217e4c00a1c
joshua onwuka <contactuche02@gmail.com>
2021-09-28 14:31:43 -0700

    defines a square uisng getter and setter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""module documentation"""
+
+
+class Square:
+    """Defines a square.
+        Private instance attribute: size.
+        Instantiate with optional size.
+        """
+    def __init__(self, size=0):
+        """Initialize the data conditions"""
+        self.__size__ = size
+
+    @property
+    def size(self):
+        """Retrieve the size."""
+        return self.__size__
+
+    @size.setter
+    def size(self, value):
+        """Sets the size to a value."""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size__ = value
+
+    def area(self):
+        """Return the current square area."""
+        return self.__size__ ** 2
