e07aacedf8a2de7bfac6262697520ea4fcda7f96
joshua onwuka <contactuche02@gmail.com>
2021-10-05 23:26:33 -0700

    prints My name is <first name> <last name>

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Defines a function that prints two names
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    prints first name and last name
+    :param first_name: 
+    :param last_name: 
+    :return: first name and last name
+    """
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) != str:
+        raise TypeError("last_name must be a string")
+    print("My name is {:s} {:s}".format(first_name, last_name))
