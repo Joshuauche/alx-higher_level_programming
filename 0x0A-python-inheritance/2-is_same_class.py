9828433334485bcd776f7c93b230fec98a624d5a
joshua onwuka <contactuche02@gmail.com>
2021-10-11 20:43:29 -0700

    instance of the specified class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Instance of a specified class
+methods:
+is_same_class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    checks if object is of same class
+    :param obj: 
+    :param a_class: 
+    :return: True or False
+    """
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
