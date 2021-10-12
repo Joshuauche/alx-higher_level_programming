13047320aea475d66a5ddf6c8d4d54483fb73354
joshua onwuka <contactuche02@gmail.com>
2021-10-11 20:48:29 -0700

    instance of the specified class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -9,8 +9,8 @@ is_same_class
 def is_same_class(obj, a_class):
     """
     checks if object is of same class
-    :param obj: 
-    :param a_class: 
+    :param obj: obj
+    :param a_class: a_class
     :return: True or False
     """
     if type(obj) is a_class:
