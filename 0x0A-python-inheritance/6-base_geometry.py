aecefde7b93e8a01ce66c7430082929d269809db
joshua onwuka <contactuche02@gmail.com>
2021-10-11 21:48:32 -0700

    raises and exception message

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/python3
+"""
+Defining BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    Empty class
+    """
+
+    def area(self):
+        """
+        Raises an exception message
+        :return: exception
+        """
+        raise Exception("area() is not implemented")
