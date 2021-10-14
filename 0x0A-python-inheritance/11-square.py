92b758cd3c9514834d613a394537625f6dc19e46
joshua onwuka <contactuche02@gmail.com>
2021-10-13 22:22:38 -0700

    fixed errors

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -20,6 +20,7 @@ class Square(Rectangle):
         :param size:
         """
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
 
     def area(self):
