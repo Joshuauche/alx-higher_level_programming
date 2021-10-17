48cde6ca006ad3915ed4295ca6d3556e1145ba07
joshua onwuka <contactuche02@gmail.com>
2021-10-17 04:12:56 -0700

    appends to file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+"""
+Appends text to a file
+Creates it, if doesn't exist
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    Appends text to a file and returns the text
+    :param filename:
+    :param text:
+    :return:return the text appended and the number of cha
+    """
+
+    with open(filename, encoding='utf-8', mode='a')as a_file:
+        a_file.write(text)
+        return a_file.tell()
