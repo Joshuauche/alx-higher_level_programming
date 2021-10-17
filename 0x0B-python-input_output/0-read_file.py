7e369c5a2f3992a977a4bf281b0718e9a94e5c7a
joshua onwuka <contactuche02@gmail.com>
2021-10-17 03:49:16 -0700

    fixed

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-""""
-Reads file
+"""Module 0-read_file.
+Reads from a file and prints.
 """
 
 
 def read_file(filename=""):
-    """
-    Reads from a file and prints
-    :param filename:
-    :return: file strings
+    """Reads from filename and prints
+    its contents to stdout.
+    Args:
+        - filename: name of the file
     """
 
-    with open(filename, encoding='utf-8', mode='r') as a_file:
-        read_text = a_file.read()
-        print(read_text)
+    with open(filename) as f:
+        read_text = f.read()
+        print(read_text, end="")
