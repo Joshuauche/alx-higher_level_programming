e0087648b4f8d5e898030f27ba17bca2c26b70b7
joshua onwuka <contactuche02@gmail.com>
2021-10-19 05:50:13 -0700

    fixed

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,26 +1,13 @@
 #!/usr/bin/python3
-"""
-module for taking arguments and adds to a list, saves to file
-"""
-
-
+""" Module """
 import sys
+from os import path
+
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
-def main():
-    """contain main code in function
-    """
-    try:
-        new_list = load_from_json_file('add_item.json')
-    except:  # file didn't exist to read from
-        new_list = []
-
-    new_list.extend([sys.argv[i] for i in range(0, len(sys.argv)) if i != 0])
-    try:
-        save_to_json_file(new_list, 'add_item.json')
-    except:
-        pass
-
-main()
+new_list = []
+if path.isfile("add_item.json"):
+    new_list = load_from_json_file("add_item.json")
+save_to_json_file(new_list + sys.argv[1:], "add_item.json")
