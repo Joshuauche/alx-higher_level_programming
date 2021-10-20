6e82cc5a1d500f293cff49961ff4739894c39b7e
joshua onwuka <contactuche02@gmail.com>
2021-10-20 03:00:02 -0700

    attribute names contained in this list must be retrieved

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Defining Class
+methods:
+def to_json(self):
+def __init__(self, first_name, last_name, age):
+"""
+
+
+class Student:
+    """
+    Class that defines a student by
+    some public instance attributes
+    """
+    first_name = ""
+    last_name = ""
+    age = 0
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Instantiation of giving parameters
+        :param first_name:
+        :param last_name:
+        :param age:
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Public method that retrieves a dictionary
+        :return: dictionary representation of a Student
+        """
+        if attrs is None:
+            return self.__dict__
+
+        json_dict = {}
+        for i in attrs:
+            if hasattr(self, i):
+                json_dict[i] = getattr(self, i)
+        return json_dict
