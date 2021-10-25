8443ecbde085ccb7451b9b738df4a36c22ff05ea
joshua onwuka <contactuche02@gmail.com>
2021-10-25 09:45:35 -0700

    used of args and kwargs

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -55,3 +55,37 @@ class Square(Rectangle):
             raise ValueError("width must be > 0")
         self.__width = value
         self.__height = value
+
+    def update(self, *args, **kwargs):
+        """
+        Square updates
+        using args and kwargs
+        """
+        new_args = [self.id, self.size, self.x, self.y]
+        if len(args) == 0 or args is None:
+            if len(kwargs) == 0:
+                return
+            else:
+                try:
+                    new_args[0] = kwargs['id']
+                except KeyError:
+                    pass
+                try:
+                    new_args[1] = kwargs['size']
+                except KeyError:
+                    pass
+                try:
+                    new_args[2] = kwargs['x']
+                except KeyError:
+                    pass
+                try:
+                    new_args[3] = kwargs['y']
+                except KeyError:
+                    pass
+        else:
+            for i in range(len(args)):
+                new_args[i] = args[i]
+        self.__init__(new_args[1],
+                      new_args[2],
+                      new_args[3],
+                      new_args[0])
