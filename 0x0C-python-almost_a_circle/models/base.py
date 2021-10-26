0a10ae115690f46a390fb18a0f73a2e7fa451f22
joshua onwuka <contactuche02@gmail.com>
2021-10-25 20:46:50 -0700

    Dictionary to Instance

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -75,7 +75,7 @@ class Base:
         if not dictionary and dictionary == {}:
             return
         if cls.__name__ == "Rectangle":
-            dummy = cls(1,1)
+            dummy = cls(1, 1)
         elif cls.__name__ == "Square":
             dummy = cls(1)
         dummy.update(**dictionary)
