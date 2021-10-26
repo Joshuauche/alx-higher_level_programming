68461e881bf6afbd4366f1384f46fb4aac506cf1
joshua onwuka <contactuche02@gmail.com>
2021-10-25 19:50:43 -0700

    JSON string representation of list_dictionaries

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,7 @@ Base class fo all other classes
 methods:
 def __init__(self, id=None):
 """
+import json
 
 
 class Base:
@@ -21,3 +22,18 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        static method which checks and
+        returns list of dictionaries to json strings
+        :return:
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        if type(list_dictionaries) != list or \
+                not all(type(i) == dict for i in list_dictionaries):
+            raise TypeError("list_dictionaries must be a list of dictionaries")
+        else:
+            return json.dumps(list_dictionaries)
