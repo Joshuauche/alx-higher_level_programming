83b6f1cc4d1185a20e9b01f4a60bdb43302901b9
joshua onwuka <contactuche02@gmail.com>
2021-11-21 20:55:14 -0800

    script that creates the table force_name on your MySQL server

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,2 +1,2 @@
 -- script that creates the table force_name on your MySQL server
-CREATE TABLE IF NOT EXISTS (id INT, name VARCHAR(256) NOT NULL);
+CREATE TABLE  (id INT, name VARCHAR(256) NOT NULL);
