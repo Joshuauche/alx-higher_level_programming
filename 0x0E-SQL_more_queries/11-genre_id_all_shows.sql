b42b08914c052f9e52b65c6a469546997e47fff8
joshua onwuka <contactuche02@gmail.com>
2021-11-22 21:54:00 -0800

    script that lists all shows contained in the database hbtn_0d_tvshows and displays null if a show doesn't have a genre

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,13 @@
+-- script that lists all shows contained in the database hbtn_0d_tvshows
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- If a show doesn’t have a genre, display NULL
+-- You can use only one SELECT statement
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+FULL JOIN tv_show_genres
+FULL JOIN tv_genres
+ON tv_shows.id = show_id
+AND tv_genres.id = genre_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id
+ASC;
