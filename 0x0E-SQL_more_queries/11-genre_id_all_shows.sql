65458113e577b07e32794948510f057311c35837
joshua onwuka <contactuche02@gmail.com>
2021-11-23 02:00:58 -0800

    modified it

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -6,6 +6,7 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres
+ON tv_shows.id = show_id
 LEFT JOIN tv_genres
 ON tv_shows.id = show_id
 AND tv_genres.id = genre_id
