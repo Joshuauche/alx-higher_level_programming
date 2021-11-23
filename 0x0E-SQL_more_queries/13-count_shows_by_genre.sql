419988d7e9133ed38b3753087993e0d60c113425
joshua onwuka <contactuche02@gmail.com>
2021-11-23 02:57:50 -0800

    script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -9,6 +9,6 @@ SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
 FROM tv_genres
 JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
-ORDER BY genre 
+GROUP BY tv_genres.name 
 ORDER BY number_of_shows
 DESC;
