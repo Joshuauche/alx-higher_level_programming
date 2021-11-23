bf96a066dff5739deb2bc83f37813f898c236c46
joshua onwuka <contactuche02@gmail.com>
2021-11-22 20:50:55 -0800

    script that lists all cities contained in the database

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -3,9 +3,9 @@
 -- Each record should display: cities.id - cities.name - states.name
 -- Results must be sorted in ascending order by cities.id
 -- You can use only one SELECT statement
-SELECT DISTINCT cities.id, cities.name, states.name
+SELECT cities.id, cities.name, states.name
 FROM cities
-JOIN state
+JOIN states
 ON cities.state_id = states.id
 ORDER BY cities.id
 ASC;
