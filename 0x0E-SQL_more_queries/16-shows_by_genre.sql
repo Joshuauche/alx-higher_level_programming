b0d526e1132fd9386e255f078345a959d2318c10
joshua onwuka <contactuche02@gmail.com>
2021-11-23 04:44:07 -0800

    script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -4,7 +4,7 @@
 -- Results must be sorted in ascending order by the show title and genre name
 -- You can use only one SELECT statement
 
-SELECT tv_shows.title, tv_show_genres.name
+SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
