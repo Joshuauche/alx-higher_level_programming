f05838df384119be904663a50785bb89357f5d81
joshua uche <contactuche02@gmail.com>
2021-12-14 22:33:09 +0000

    script that prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing size');
-}else {
+} else {
   const num = Number(process.argv[2]);
   for (let i = 0; i < num; i++) {
     console.log('X'.repeat(num));
