6e900d790df0999328d68b114d866ed666a82ee9
joshua uche <contactuche02@gmail.com>
2021-12-14 16:35:31 +0000

    fixed

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
+let myVar1 = 'C is fun';
+let myVar2 = 'Python is cool';
+let myVar3 = 'JavaScript is amazing';
+console.log(myVar1);
+console.log(myVar2);
+console.log(myVar3);
