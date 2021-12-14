778da289b4587f729817be006926eb90ab410d94
joshua uche <contactuche02@gmail.com>
2021-12-14 22:35:21 +0000

    script that prints x times C is fun

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const num = parseInt(process.argv[2]);
+const num = Number(process.argv[2]);
 if (num === undefined || isNaN(num)) {
   console.log('Missing number of occurrences');
 } else {
