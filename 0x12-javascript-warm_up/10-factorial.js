036d39e67f94e15efb6ca970d5189e283ac7c33a
joshua uche <contactuche02@gmail.com>
2021-12-15 04:13:13 +0000

    script that computes and prints a factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 function factorial (num) {
   if (num < 0) {
-    return(-1);
-  } 
-  if (num === 0 || num === 1) {
-    return(1);
+    return (-1);
   }
-  return (n * factorial(num - 1));
+  if (num === 0 || num === 1) {
+    return (1);
+  }
+  return (num * factorial(num - 1));
 }
 
 console.log(factorial(Number(process.argv[2])));
