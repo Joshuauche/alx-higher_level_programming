101ec260d6d24b50e0f37ffe15cb53577b6ab544
joshua uche <contactuche02@gmail.com>
2021-12-15 05:17:41 +0000

    script that searches the second biggest integer in the list of arguments

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+if (process.argv.length <= 2) {
+  console.log(0);
+} else {
+  const num = process.argv;
+  let biggest = 0;
+  for (let i = 0; i < num.length; i++) {
+    if (num[i] > biggest) {
+      biggest = num[i];
+    }
+  }
+  console.log(parseInt(biggest));
+}
