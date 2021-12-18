4a0efe69432e074cae3f24613171420d36b9d644
joshua uche <contactuche02@gmail.com>
2021-12-18 17:39:54 +0000

    converts a number from base 10 to another base passed as argument

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,4 +1,9 @@
 #!/usr/bin/node
+/*
+Prototype: exports.converter = function (base)
+You are not allowed to import any file
+You are not allowed to declare any new variable (var, let, etc..)
+*/
 exports.converter = function (base) {
   // using closure
   return function (num) {
