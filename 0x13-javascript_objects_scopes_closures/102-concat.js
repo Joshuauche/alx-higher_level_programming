f5211b1582fc2330fcc68b37d73ab5dd86566256
joshua uche <contactuche02@gmail.com>
2021-12-18 23:16:53 +0000

    script that concats 2 files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -4,7 +4,7 @@ The first argument is the file path of the first source file
 The second argument is the file path of the second source file
 The third argument is the file path of the destination
 */
-let files = reqiure('files');
+const files = reqiure('files');
 const file1 = files.readFileSync(process.argv[2], 'utf8');
 const file2 = files.readFileSync(process.argv[3], 'utf8');
 files.writeFileSync(process.argv[4], file1 + file2);
