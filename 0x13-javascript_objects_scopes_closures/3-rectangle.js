3c5033be1ec69654ee0d056c69e728f5186f1993
joshua uche <contactuche02@gmail.com>
2021-12-18 09:12:14 +0000

    instance method called print() that prints the rectangle using the character X

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-/* 
+/*
 You must use the class notation for defining your class.
 The constructor must take 2 arguments w and h.
 Initialize the instance attribute width with the value of w.
