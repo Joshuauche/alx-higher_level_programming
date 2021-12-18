4e6657407b50b4ff66f10b165af96b08099f2c3b
joshua uche <contactuche02@gmail.com>
2021-12-18 10:11:07 +0000

    class Square that defines a square and inherits from Rectangle

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-/* 
+/*
 You must use the class notation for defining your class and extends
 The constructor must take 1 argument: size
 The constructor of Rectangle must be called (by using super())
