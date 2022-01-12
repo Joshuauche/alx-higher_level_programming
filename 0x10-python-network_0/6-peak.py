cd3a9474d625cb0bd57092316cfa07ce7f32aa47
joshua uche <contactuche02@gmail.com>
2022-01-12 03:35:31 +0000

    finds a peak in a list of unsorted integers

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -11,3 +11,11 @@ def find_peak(list_of_integers):
     if list_of_integers:
         list_of_integers.sort(reverse=True)
         return list_of_integers[0]
+
+    # max = 0
+    # if len(list_of_integers) < 1:
+    #     return None
+    # for x in list_of_integers:
+    #     if x > max:
+    #         max = x
+    # return max
