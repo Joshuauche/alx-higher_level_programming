383446b7cc0973535625a95a5714538d82098f81
joshua uche <contactuche02@gmail.com>
2022-01-13 11:23:56 +0000

    fixed it

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
-curl -s -d '{"name":"John Doe", "age":33}' -X POST "$1" 
+curl -s -d @"$2" -X POST "$1" -H "COntent-type: application/json"
