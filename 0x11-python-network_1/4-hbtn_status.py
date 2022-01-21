a80a91a721783ce1b9703636c820bf10c8f3624c
joshua uche <contactuche02@gmail.com>
2022-01-21 18:48:37 +0000

    script that fetches url status in str format

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+script that fetches url status in str format
+"""
+
+import requests
+
+if __name__ == "__main__":
+    req = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t - type: {}".format(type(req.text)))
+    print("\t - content: {}".format(req.text))
