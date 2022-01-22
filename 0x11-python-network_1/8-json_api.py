3d27dd336df831896aeb8f95ab8ffd0ec42f3059
joshua uche <contactuche02@gmail.com>
2022-01-22 13:12:22 +0000

    search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""
+script that takes in a letter and 
+sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+
+The letter must be sent in the variable q
+If no argument is given, set q=""
+display the id and name like this: [<id>] <name>
+
+Otherwise:
+Display Not a valid JSON if the JSON is invalid
+Display No result if the JSON is empty
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        set_q = {"q": ""}
+    else:
+        set_q = {"q": sys.argv[1]}
+    req = requests.post("http://0.0.0.0:5000/search_user", data=set_q)
+    try:
+        respons = req.json()
+        if respons != {}:
+            print("[{}] {}".format(respons["id"], respons["name"]))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
+    
