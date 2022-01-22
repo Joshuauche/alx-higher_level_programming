61e59f0ce6b26570858689f097329836aa305cb8
joshua uche <contactuche02@gmail.com>
2022-01-22 11:00:11 +0000

    ends a POST request to the passed URL

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -12,6 +12,6 @@ import sys
 
 if __name__ == "__main__":
     data = {'email', sys.argv[2]}
-    req = requests.post(sys.argv[1], data = data)
+    req = requests.post(sys.argv[1], data=data)
     respons = req.text
     print("{}".format(respons))
