7eb58c4575168b3861605564a08e5ad03fee87db
joshua uche <contactuche02@gmail.com>
2022-01-22 14:20:44 +0000

    script that list commit with name from a repo

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+The first argument will be the repository name
+The second argument will be the owner name
+You must use the GitHub API,
+here is the documentation https://developer.github.com/v3/repos/commits/
+Print all commits by: `<sha>: <author name>` (one by line)
+Please list 10 commits
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    req = requests.get("https://developer.github.com/v3/repos/commits/",
+                       auth=(sys.argv[1], sys.argv[2]))
+    resp = req.json()
+
+    for i in resp[:10]:
+        print(i.get('sha'), end=": ")
+        print(i.get('commit').get('author').get('name'))
